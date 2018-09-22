080a7bb450dd77d874a99da24c0d01d95fdeb5ee
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2018-09-22 19:40:39 +0900

    udp

--- chapter7_UDPSocket/udp_socket_server.go
+++ chapter7_UDPSocket/udp_socket_server.go
@@ -1,1 +1,30 @@
-package chapter7_UDPSocket
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	fmt.Println("Server is running at localhost:8888")
+	conn, err := net.ListenPacket("udp", "localhost:8888")
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	buffer := make([]byte, 1500)
+	for {
+		length, remoteAddres, err := conn.ReadFrom(buffer)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("Received from %v: %v\n", remoteAddres, string(buffer[:length]))
+		_, err = conn.WriteTo([]byte("hello from server"), remoteAddres)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+}
