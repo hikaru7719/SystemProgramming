f79a704c1977bfcffa38777b3f59bdb4788a8bca
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2018-09-22 19:41:09 +0900

    finish chapter9_FileSystem

---
+++ chapter9_FileSystem/file.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func open() {
+	file, err := os.Create("textfile.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	io.WriteString(file, "New file content\n")
+}
+
+func read() {
+	file, err := os.Open("textfile.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	fmt.Println("Read file:")
+	io.Copy(os.Stdout, file)
+}
+
+func append() {
+	file, err := os.OpenFile("textfile.txt", os.O_RDWR|os.O_APPEND, 0666)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	io.WriteString(file, "Append content\n")
+}
+
+func main() {
+	open()
+	read()
+	append()
+	os.Mkdir("setting", 0755)
+	os.MkdirAll("setting/myapp/networksettings", 0755)
+}
